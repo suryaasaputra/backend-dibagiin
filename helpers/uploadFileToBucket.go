3987b12a3308558d718819037345bfca825cf3ea
suryaasaputra <suryaaasaputra.s@gmail.com>
2022-11-05 13:14:02 +0700

    update credentials

--- helpers/uploadFileToBucket.go
+++ helpers/uploadFileToBucket.go
@@ -2,9 +2,11 @@ package helpers
 
 import (
 	"dibagi/config"
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
+	"os"
 	"time"
 
 	"cloud.google.com/go/storage"
@@ -17,8 +19,11 @@ func UploadToBucket(c *http.Request, key, id string) (string, error) {
 		storageClient *storage.Client
 		err           error
 	)
+	credentials := os.Getenv("STORAGE_CREDENTIALS")
+	jsonCredentials, _ := json.Marshal(credentials)
 	ctx := appengine.NewContext(c)
-	storageClient, err = storage.NewClient(ctx, option.WithCredentialsFile("../secret/dibagi-in-key.json"))
+	// storageClient, err = storage.NewClient(ctx, option.WithCredentialsFile("dibagi-in-key.json")
+	storageClient, err = storage.NewClient(ctx, option.WithCredentialsJSON(jsonCredentials))
 	if err != nil {
 		return "", err
 	}
